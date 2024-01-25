f8d73484c98b40870eb2c1ee32a6ebbfb6a60db8
Mario <mcg00@iesemilidarder.com>
2024-01-25 18:21:33 +0100

    Primera Version

--- HolaMon.java
+++ HolaMon.java
@@ -13,6 +13,6 @@ public class HolaMon {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        System.out.println("Hola mon");
     }
 }
